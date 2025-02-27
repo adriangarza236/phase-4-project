76078a27d1be6909c22dab12175673ee2b3a44c0
Adrian Garza <adriangarza236@yahoo.com>
2025-02-27 09:29:26 -0600

    initial commit

---
+++ server/migrations/versions/32ad51d89ea4_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 32ad51d89ea4
+Revises: 
+Create Date: 2025-02-27 09:29:18.462456
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '32ad51d89ea4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
