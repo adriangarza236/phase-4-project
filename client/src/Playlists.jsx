3b3979ef93074cfbaf23fef20d900270617578dc
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-02-28 16:00:04 -0800

    Finish CLside Routes and Forms

--- client/src/Playlists.jsx
+++ client/src/Playlists.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
 const Playlists = () => {
+  const [playlists, setPlaylists] = useState([]); 
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5555/api/playlists") 
+      .then((response) => response.json()) 
+      .then((data) => setSongs(data)) 
+  }, []);
+
   return (
-    <div>Playlists</div>
-  )
-}
+    <div>
+      <h2>🎵 Playlists</h2>
+      <ul>
+        {playlists.length > 0 ? (
+          playlists.map((playlist) => (
+            <li key={playlist.id}>
+              {playlist.title} - {playlist.songs} - {song.vibe}
+            </li>
+          ))
+        ) : (
+          <p>Loading songs...</p>
+        )}
+      </ul>
+    </div>
+  );
+};
+
 
 export default Playlists
--- client/src/Songs.jsx
+++ client/src/Songs.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 
 const Songs = () => {
-  return 
-    
-  
-}
+  const [songs, setSongs] = useState([]); 
 
-export default Songs
+  useEffect(() => {
+    fetch("http://127.0.0.1:5555/api/songs") 
+      .then((response) => response.json()) 
+      .then((data) => setSongs(data)) 
+  }, []);
+
+  return (
+    <div>
+      <h2>🎵 Song List</h2>
+      <ul>
+        {songs.length > 0 ? (
+          songs.map((song) => (
+            <li key={song.id}>
+              {song.title} - {song.artist} - {song.playlist_songs.vibe} - <img src={song.album_cover} alt="Album Cover" />
+            </li>
+          ))
+        ) : (
+          <p>Loading songs...</p>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Songs;
