c31f8bb190acab0f4d89ee3c69cf1d14dc1a6457
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-03-03 10:21:03 -0800

    Adds Song form to DB

--- client/src/SongForm.jsx
+++ client/src/SongForm.jsx
@@ -3,74 +3,81 @@ import { useFormik } from 'formik'
 import { useEffect, useState } from 'react'
 import React from 'react'
 
-
-// []TODO Add vibes to song form
-// []TODO Take drilled props and convert to context
-// []TODO Smile :)
-
-
 const SongForm = () => {
-const [formData, setFormData] = useState({
-  albumCover: "",
-  title: "",
-  album: "",
-  vibe: "",
-})
-console.log(formData)
+  const [formData, setFormData] = useState({
+    albumCover: "",
+    title: "",
+    artist: "",
+    album: ""
+  });
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-
-const handleChange = (event) => {
-  const {name, value} = event.target
-  setFormData({...formData, [name]: value})
-}
-
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-    const newMovie = {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newSong = {
       title: formData.title,
-      image: formData.image,
-      director: formData.director,
-      rating: formData.rating
+      artist: formData.artist,
+      album_cover: formData.albumCover,
+      album: formData.album
     };
-    handlePlaylistForm(newMovie);
-    setFormData({ title: "", image: "", director: "", rating: "" });
-  }
 
+    fetch("http://127.0.0.1:5555/api/songs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newSong)
+    })
+    .then((response) => response.json())
+    .then((addedSong) => {
+      console.log("Song added:", addedSong);
+      setFormData({ albumCover: "", title: "", artist: "", album: ""});
+    })
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Album Cover:
         <input 
-        type="text" 
-        name="image"
-        value={formData.image}
-        onChange={handleChange} />
+          type="text" 
+          name="albumCover"
+          value={formData.albumCover}
+          onChange={handleChange} 
+        />
       </label>
       <label>
         Title:
         <input 
-        type="text" 
-        name="title"
-        value={formData.title} 
-        onChange={handleChange}/>
+          type="text" 
+          name="title"
+          value={formData.title} 
+          onChange={handleChange}
+        />
       </label>
       <label>
-        Vibe:
+        Artist:
         <input 
-        type="text" 
-        name="vibe"
-        value={formData.title} 
-        onChange={handleChange}/>
+          type="text" 
+          name="artist"
+          value={formData.artist} 
+          onChange={handleChange}
+        />
       </label>
-      
-     
+      <label>
+        Album:
+        <input 
+          type="text" 
+          name="album"
+          value={formData.album} 
+          onChange={handleChange}
+        />
+      </label>
       <button type="submit">Add song!</button>
     </form>
-  )
+  );
+};
 
-}
-
-export default SongForm
+export default SongForm;
